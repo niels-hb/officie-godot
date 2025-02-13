deccb5a04f5fd39c228a516460c6b8f197de6161
Niels Heidbrink <72007270+niels-hb@users.noreply.github.com>
2025-02-13 11:54:05 +0700

    Added SpriteTimedState

---
+++ shared/fsm/sprite_timed_state.gd
@@ -0,0 +1,30 @@
+extends TimedState
+class_name SpriteTimedState
+
+@export var texture_on_enter: Texture
+@export var texture_on_exit: Texture
+
+@export var sprite: NodePath
+@onready var sprite_node: Sprite2D = get_node(sprite)
+
+
+func _ready() -> void:
+	super._ready()
+
+	assert(texture_on_enter != null)
+
+	assert(sprite != null)
+	assert(sprite_node != null)
+
+
+func enter(_msg: Dictionary = {}) -> void:
+	sprite_node.texture = texture_on_enter
+
+	await super.enter(_msg)
+
+
+func exit() -> void:
+	if texture_on_exit != null:
+		sprite_node.texture = texture_on_exit
+
+	super.exit()
