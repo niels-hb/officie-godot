34ecfab1705fbfb64ed60ca1d056047358fbc66e
Niels Heidbrink <72007270+niels-hb@users.noreply.github.com>
2025-02-13 11:54:06 +0700

    Added disabled state to RoomPlacementControls OkButton

--- ui/room_placement_controls/room_placement_controls.gd
+++ ui/room_placement_controls/room_placement_controls.gd
@@ -5,6 +5,10 @@ signal confirm
 signal cancel
 
 
+func set_confirm_button(disabled: bool) -> void:
+	($HBoxContainer/OkMarginContainer/OkButton as TextureButton).disabled = disabled
+
+
 func _on_ok_button_pressed() -> void:
 	confirm.emit()
 
