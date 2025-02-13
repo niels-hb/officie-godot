fadf82d8d7a18e726b37e611f73f56ae62348ba3
Niels Heidbrink <72007270+niels-hb@users.noreply.github.com>
2025-02-13 11:54:05 +0700

    Removed time setting fields from Room, will set on state instead

--- static_config/room_configuration/room.gd
+++ static_config/room_configuration/room.gd
@@ -4,28 +4,21 @@ var name: String
 var size: Vector2i
 var price: int
 var income_per_day: int
+var time_to_build: Vector2i
 var time_until_occupied: Vector2i
 var time_until_collectable: Vector2i
+var time_until_cleaned: Vector2i
 var icon: String
 var scene: PackedScene
 
 
 func _init(
-	_name: String,
-	_size: Vector2i,
-	_price: int,
-	_income_per_day: int,
-	_time_until_occupied: Vector2i,
-	_time_until_collectable: Vector2i,
-	_icon: String,
-	_scene: String
+	_name: String, _size: Vector2i, _price: int, _income_per_day: int, _icon: String, _scene: String
 ) -> void:
 	self.name = _name
 	self.size = _size
 	self.price = _price
 	self.income_per_day = _income_per_day
-	self.time_until_occupied = _time_until_occupied
-	self.time_until_collectable = _time_until_collectable
 	self.icon = _icon
 	self.scene = load(_scene)
 
--- static_config/room_configuration/room_configuration.gd
+++ static_config/room_configuration/room_configuration.gd
@@ -7,8 +7,6 @@ const ROOM_NAME_IDENTIFIER: String = "room_name"
 const ROOM_SIZE_IDENTIFIER: String = "size"
 const ROOM_PRICE_IDENTIFIER: String = "price"
 const ROOM_INCOME_PER_DAY_DENTIFIER: String = "income_per_day"
-const ROOM_TIME_UNTIL_OCCUPIED_DENTIFIER: String = "time_until_occupied"
-const ROOM_TIME_UNTIL_COLLECTABLE_DENTIFIER: String = "time_until_collectable"
 const ROOM_ICON_IDENTIFIER: String = "icon"
 const ROOM_SCENE_IDENTIFIER: String = "scene"
 
@@ -33,24 +31,7 @@ func _ready() -> void:
 		var room_size: Vector2i = room_configuration.get_value(room, ROOM_SIZE_IDENTIFIER)
 		var price: int = room_configuration.get_value(room, ROOM_PRICE_IDENTIFIER)
 		var income_per_day: int = room_configuration.get_value(room, ROOM_INCOME_PER_DAY_DENTIFIER)
-		var time_until_occupied: Vector2i = room_configuration.get_value(
-			room, ROOM_TIME_UNTIL_OCCUPIED_DENTIFIER
-		)
-		var time_until_collectable: Vector2i = room_configuration.get_value(
-			room, ROOM_TIME_UNTIL_COLLECTABLE_DENTIFIER
-		)
 		var icon: String = room_configuration.get_value(room, ROOM_ICON_IDENTIFIER)
 		var scene: String = room_configuration.get_value(room, ROOM_SCENE_IDENTIFIER)
 
-		available_rooms.append(
-			Room.new(
-				room_name,
-				room_size,
-				price,
-				income_per_day,
-				time_until_occupied,
-				time_until_collectable,
-				icon,
-				scene
-			)
-		)
+		available_rooms.append(Room.new(room_name, room_size, price, income_per_day, icon, scene))
