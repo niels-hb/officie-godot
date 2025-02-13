ef979ee31534610c26b4c8b20d3af03c00033bce
Niels Heidbrink <72007270+niels-hb@users.noreply.github.com>
2025-02-13 11:54:06 +0700

    Updated BaseStaff to automatically target room, added CleaningStaff

--- scenes/staff/base_staff/base_staff.gd
+++ scenes/staff/base_staff/base_staff.gd
@@ -1,13 +1,30 @@
 extends CharacterBody2D
+class_name BaseStaff
 
-var movement_speed: float = 200.0
+@export_range(1, 500, 1) var movement_speed: float
+
+@export var assignable_states: Array[String]
 
 @onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
+
+var staff_room: StaffRoom
+
+var assigned_room: BaseRoom
 
 var ready_to_navigate: bool = false
 
 
+func init(_staff_room: StaffRoom) -> BaseStaff:
+	staff_room = _staff_room
+
+	return self
+
+
 func _ready() -> void:
+	assert(!assignable_states.is_empty())
+	assert(staff_room != null)
+	assert(staff_room is StaffRoom)
+
 	call_deferred("_actor_setup")
 
 
@@ -17,8 +34,12 @@ func _actor_setup() -> void:
 
 
 func _physics_process(_delta: float) -> void:
+	assigned_room = _find_assignable_room()
+
 	if ready_to_navigate:
-		navigation_agent.target_position = get_global_mouse_position()
+		navigation_agent.target_position = (
+			assigned_room.position if assigned_room != null else staff_room.position
+		)
 
 	if navigation_agent.is_navigation_finished():
 		return
@@ -29,3 +50,14 @@ func _physics_process(_delta: float) -> void:
 	velocity = current_agent_position.direction_to(next_path_position) * movement_speed
 
 	var _result: bool = move_and_slide()
+
+
+func _find_assignable_room() -> BaseRoom:
+	if assigned_room != null:
+		return assigned_room
+
+	for room: BaseRoom in staff_room.office.rooms.get_children():
+		if assignable_states.has(room.state_machine.state.name):
+			return room
+
+	return null
