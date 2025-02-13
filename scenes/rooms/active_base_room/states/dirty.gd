88f9725f41ef2f97d7ef0bd86ae0ea86f3b3e646
Niels Heidbrink <72007270+niels-hb@users.noreply.github.com>
2025-02-13 11:54:06 +0700

    Added custom Dirty state to ActiveBaseRoom

---
+++ scenes/rooms/active_base_room/states/dirty.gd
@@ -0,0 +1,18 @@
+extends State
+
+@export_range(0, 100, 1) var cleaning_time: int
+
+
+func _ready() -> void:
+	assert(cleaning_time > 0)
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if (body as BaseStaff).assigned_room == owner:
+		print("%s entered. Cleaning for %d seconds." % [body, cleaning_time])
+
+		var timer: SceneTreeTimer = get_tree().create_timer(cleaning_time)
+		await timer.timeout
+
+		(body as BaseStaff).assigned_room = null
+		state_machine.transition_to("Ready")
