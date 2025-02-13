16180439bcaca098af3cf21de07afbba716bf028
Niels Heidbrink <72007270+niels-hb@users.noreply.github.com>
2025-02-13 11:54:05 +0700

    Removed BaseOffice/Grid/Cells and GridOutline

--- scenes/offices/base_office/base_office.gd
+++ scenes/offices/base_office/base_office.gd
@@ -1,21 +1,7 @@
 extends Node2D
 class_name BaseOffice
 
-const grid_outline: PackedScene = preload("res://scenes/offices/base_office/grid_outline.tscn")
-
 @onready var camera: GestureControlledCamera2D = $Camera
-
-# TODO: Dynamic row/column count based on office floor plan
-var office_size: Vector2 = Vector2(19, 33)
-
-
-func _ready() -> void:
-	for i: int in range(0, office_size.x):
-		for j: int in range(0, office_size.y):
-			var cell: Node2D = grid_outline.instantiate()
-			cell.position.x = i * GameInformation.GRID_SIZE
-			cell.position.y = j * GameInformation.GRID_SIZE
-			$Grid/Cells.add_child(cell)
 
 
 func _on_room_selector_room_selected(room: Room) -> void:
@@ -23,7 +9,7 @@ func _on_room_selector_room_selected(room: Room) -> void:
 		return
 
 	var base_room: BaseRoom = (room.scene.instantiate() as BaseRoom).init(self, room)
-	base_room.position = Vector2i(office_size / 2) * GameInformation.GRID_SIZE
+	base_room.position = Vector2i(10, 20) * GameInformation.GRID_SIZE
 	var _connect_result: int = base_room.drag_state_changed.connect(_on_drag_state_changed)
 
 	$Grid/Rooms.add_child(base_room)
@@ -31,7 +17,6 @@ func _on_room_selector_room_selected(room: Room) -> void:
 
 func _on_drag_state_changed(dragging: bool) -> void:
 	_toggle_camera_movement(dragging)
-	($Grid/Cells as Node2D).visible = dragging
 
 
 func _toggle_camera_movement(disabled: bool) -> void:
