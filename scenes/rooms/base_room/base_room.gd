866206d76443141623591158af18c1fa3bd7057f
Niels Heidbrink <72007270+niels-hb@users.noreply.github.com>
2025-02-13 11:54:06 +0700

    BaseRoom: Removed BuyControls, will be moved to global UI

--- scenes/rooms/base_room/base_room.gd
+++ scenes/rooms/base_room/base_room.gd
@@ -5,7 +5,6 @@ signal drag_state_changed(dragging: bool)
 
 @onready var collison_shape: CollisionShape2D = $CollisionShape2D
 @onready var image: Sprite2D = $Image
-@onready var buy_controls: Node2D = $BuyControls
 @onready var state_machine: StateMachine = $StateMachine
 
 var office: BaseOffice
@@ -43,18 +42,6 @@ func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> voi
 		_on_dragging()
 
 
-func _on_ok_button_pressed() -> void:
-	if !_validate_position() or GameInformation.money < room.price:
-		return
-
-	GameInformation.money -= room.price
-	state_machine.transition_to("Building")
-
-
-func _on_cancel_button_pressed() -> void:
-	queue_free()
-
-
 func _on_drag_state_changed(dragging: bool) -> void:
 	var scale_while_dragging: float = 4.0 if dragging else 1.0
 	collison_shape.scale = Vector2(scale_while_dragging, scale_while_dragging)
@@ -70,10 +57,6 @@ func _on_dragging() -> void:
 	)
 
 	global_position = global_mouse_position - snap_to_grid_offset
-
-	($BuyControls/ColorRect as ColorRect).modulate = (
-		Color(1, 1, 1, 0.5) if _validate_position() else Color(1, 0, 0, 1)
-	)
 
 
 func _validate_position() -> bool:
--- scenes/rooms/base_room/states/inactive.gd
+++ scenes/rooms/base_room/states/inactive.gd
@@ -1,13 +1,1 @@
 extends State
-
-
-func enter(_msg: Dictionary = {}) -> void:
-	super.enter()
-
-	(owner as BaseRoom).buy_controls.visible = true
-
-
-func exit() -> void:
-	super.exit()
-
-	(owner as BaseRoom).buy_controls.visible = false
