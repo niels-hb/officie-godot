2f9995ee349bf0547497db3c93e025a8e3fe9b1f
Niels Heidbrink <72007270+niels-hb@users.noreply.github.com>
2025-02-13 11:54:05 +0700

    RoomSelector: Updated variables names

--- ui/room_selector/room_selector.gd
+++ ui/room_selector/room_selector.gd
@@ -1,15 +1,15 @@
 extends CanvasLayer
 
-var room_icon: PackedScene = preload("res://ui/room_selector/room_icon.tscn")
+var room_icon_scene: PackedScene = preload("res://ui/room_selector/room_icon.tscn")
 
 signal room_selected(room: Room)
 
 
 func _ready() -> void:
 	for room: Room in RoomConfiguration.available_rooms:
-		var _room_icon: RoomIcon = (room_icon.instantiate() as RoomIcon).init(room)
-		var _connect_result: int = _room_icon.selected.connect(_on_room_selected)
-		$Background/ScrollContainer/MarginContainer/AvailableRooms.add_child(_room_icon)
+		var room_icon: RoomIcon = (room_icon_scene.instantiate() as RoomIcon).init(room)
+		var _connect_result: int = room_icon.selected.connect(_on_room_selected)
+		$Background/ScrollContainer/MarginContainer/AvailableRooms.add_child(room_icon)
 
 
 func _on_room_selected(room: Room) -> void:
